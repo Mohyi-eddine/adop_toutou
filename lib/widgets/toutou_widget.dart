6b8f1be3b06c5870b468d4b5e934e9b171320d7b
Mohyi-eddine <Mohyeddone@gmail.com>
2025-09-30 14:36:36 +0200

    final version 2

--- lib/widgets/toutou_widget.dart
+++ lib/widgets/toutou_widget.dart
@@ -13,60 +13,102 @@ class ToutouWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(12),
-      width: double.infinity,
-      height: 200,
-      child: Stack(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(16),
-              boxShadow: [
-                BoxShadow(
-                  color: AppColors.black.withOpacity(0.26),
-                  blurRadius: 10,
-                  offset: const Offset(0, 5),
+    return GestureDetector(
+      onTap: () {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return Dialog(
+              backgroundColor: Colors.transparent,
+              insetPadding: const EdgeInsets.all(10),
+              child: Stack(
+                alignment: Alignment.center,
+                children: <Widget>[
+                  Image.network(
+                    toutou.imageUrl,
+                    fit: BoxFit.contain,
+                    loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
+                      if (loadingProgress == null) return child;
+                      return Center(
+                        child: CircularProgressIndicator(
+                          value: loadingProgress.expectedTotalBytes != null
+                              ? loadingProgress.cumulativeBytesLoaded / loadingProgress.expectedTotalBytes!
+                              : null,
+                        ),
+                      );
+                    },
+                  ),
+                  Positioned(
+                    top: 0,
+                    right: 0,
+                    child: IconButton(
+                      icon: const Icon(Icons.close, color: Colors.white, size: 30),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      },
+      child: Container(
+        margin: const EdgeInsets.all(12),
+        width: double.infinity,
+        height: 200,
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                boxShadow: [
+                  BoxShadow(
+                    color: AppColors.black.withOpacity(0.26),
+                    blurRadius: 10,
+                    offset: const Offset(0, 5),
+                  ),
+                ],
+                image: DecorationImage(
+                  image: NetworkImage(toutou.imageUrl),
+                  fit: BoxFit.cover,
                 ),
-              ],
-              image: DecorationImage(
-                image: NetworkImage(toutou.imageUrl),
-                fit: BoxFit.cover,
               ),
             ),
-          ),
-          Positioned(
-            top: -30,
-            left: -30,
-            child: SizedBox(
-              width: 190,
-              height: 150,
-              child: CustomPaint(
-                painter: CloudPainter(color: AppColors.playfulOrange.withOpacity(0.9)),
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        toutou.name,
-                        style: AppTextStyles.cardTitle.copyWith(color: AppColors.white),
-                      ),
-                      Text(
-                        '${toutou.age} years old',
-                        style: AppTextStyles.bodyText.copyWith(color: AppColors.white),
-                      ),
-                    ],
+            Positioned(
+              top: -30,
+              left: -30,
+              child: SizedBox(
+                width: 190,
+                height: 150,
+                child: CustomPaint(
+                  painter: CloudPainter(color: AppColors.playfulOrange.withOpacity(0.9)),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          toutou.name,
+                          style: AppTextStyles.cardTitle.copyWith(color: AppColors.white),
+                        ),
+                        Text(
+                          '${toutou.age} years old',
+                          style: AppTextStyles.bodyText.copyWith(color: AppColors.white),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          Positioned(
-            bottom: 10,
-            right: 10,
-            child: LoveMeButton(toutou: toutou),
-          ),
-        ],
+            Positioned(
+              bottom: 10,
+              right: 10,
+              child: LoveMeButton(toutou: toutou),
+            ),
+          ],
+        ),
       ),
     );
   }
